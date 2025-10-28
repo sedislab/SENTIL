ee180c6a7839c966f041636b0d5cd69cf433049c
Paapa Quansah <officialpaapa@gmail.com>
2025-10-28 17:09:29 -0500

    version() over sentil_version

--- sentil-jl/src/Sentil.jl
+++ sentil-jl/src/Sentil.jl
@@ -8,4 +8,16 @@ function __init__()
     libsentil[] = _Loader.resolve()
 end
 
+"""The version of the SENTIL core as `(major, minor, patch)`."""
+function version()
+    major = Ref{UInt32}(0)
+    minor = Ref{UInt32}(0)
+    patch = Ref{UInt32}(0)
+    ccall((:sentil_version, libsentil[]), Cvoid,
+          (Ptr{UInt32}, Ptr{UInt32}, Ptr{UInt32}), major, minor, patch)
+    return (Int(major[]), Int(minor[]), Int(patch[]))
+end
+
+export version
+
 end
