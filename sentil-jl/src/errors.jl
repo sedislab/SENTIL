8dfebc91ea9923fa0788a5054f7de52959517a7e
Paapa Quansah <officialpaapa@gmail.com>
2025-10-30 23:53:50 -0500

    funnel C error codes and the thread-local message into typed exceptions

--- sentil-jl/src/errors.jl
+++ sentil-jl/src/errors.jl
@@ -45,3 +45,38 @@ end
 Base.showerror(io::IO, e::SentilError) = print(io, nameof(typeof(e)), '(', e.code, "): ", e.msg)
 
 export SentilError, ParseError, SemanticError, EvaluationError
+
+function _last_error_message()
+    n = ccall((:sentil_get_last_error_message, libsentil[]), Csize_t,
+              (Ptr{UInt8}, Csize_t), C_NULL, 0)
+    n == 0 && return ""
+    buf = Vector{UInt8}(undef, n)
+    ccall((:sentil_get_last_error_message, libsentil[]), Csize_t,
+          (Ptr{UInt8}, Csize_t), buf, n)
+    return GC.@preserve buf unsafe_string(pointer(buf))
+end
+
+_last_error_code() =
+    SentilErrorCode(ccall((:sentil_get_last_error_code, libsentil[]), Int32, ()))
+
+function _error(code::SentilErrorCode, msg::AbstractString)
+    if code == SENTIL_ERR_PARSE
+        ParseError(code, msg)
+    elseif code == SENTIL_ERR_UNKNOWN_VARIABLE || code == SENTIL_ERR_NOT_PROBABILISTIC ||
+           code == SENTIL_ERR_UNSUPPORTED
+        SemanticError(code, msg)
+    else
+        EvaluationError(code, msg)
+    end
+end
+
+function check_error(code::Integer)
+    c = SentilErrorCode(code)
+    c == SENTIL_OK && return nothing
+    throw(_error(c, _last_error_message()))
+end
+
+function _raise_last()
+    code = _last_error_code()
+    throw(_error(code == SENTIL_OK ? SENTIL_ERR_PANIC : code, _last_error_message()))
+end
