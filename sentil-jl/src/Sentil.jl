4affd93eed5cc272191fd72b602d8b03fff50d44
Paapa Quansah <officialpaapa@gmail.com>
2025-10-30 23:44:34 -0500

    error code enum mirroring the C abi

--- sentil-jl/src/Sentil.jl
+++ sentil-jl/src/Sentil.jl
@@ -8,6 +8,8 @@ function __init__()
     libsentil[] = _Loader.resolve()
 end
 
+include("errors.jl")
+
 """The version of the SENTIL core as `(major, minor, patch)`."""
 function version()
     major = Ref{UInt32}(0)
