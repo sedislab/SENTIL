7e2f9a47684ab635427b7e13837df0eadabec7f0
Paapa Quansah <officialpaapa@gmail.com>
2025-11-05 19:25:55 -0600

    wrong include order broke robustness, pull trace ahead of formula

--- sentil-jl/src/Sentil.jl
+++ sentil-jl/src/Sentil.jl
@@ -12,8 +12,8 @@ include("errors.jl")
 include("enums.jl")
 include("value_structs.jl")
 include("handles.jl")
-include("formula.jl")
 include("trace.jl")
+include("formula.jl")
 
 """The version of the SENTIL core as `(major, minor, patch)`."""
 function version()
