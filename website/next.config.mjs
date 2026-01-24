b1967451f6954ceac740302b15bb6df598bbf65e
Paapa Quansah <officialpaapa@gmail.com>
2026-01-23 23:47:41 -0600

    static export to out/ and no image optimizer

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -2,6 +2,13 @@ import { createMDX } from 'fumadocs-mdx/next';
 
 const withMDX = createMDX();
 
-const config = {};
+const config = {
+  output: 'export',
+  images: { unoptimized: true },
+  reactStrictMode: true,
+  experimental: {
+    cpus: 2,
+  },
+};
 
 export default withMDX(config);
